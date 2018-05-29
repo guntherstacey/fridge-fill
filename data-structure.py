8a16859344c6d256b7e6508eeed0e5130f50f29b
Your Name <you@example.com>
2018-05-29 11:25:51 -0400

    Reset program to read_file code only

--- data-structure.py
+++ data-structure.py
@@ -2,22 +2,11 @@ def read_file(text_file):
   if ".txt" in text_file:
     recipe_list = open(text_file)
     opened_recipe_list = (recipe_list.read())
-    return opened_recipe_list
+    print opened_recipe_list
   else:
     print ("Invalid Input: Not a .txt file")
 
 
 
-def ingredients(string_input):
-	list = [string_input.split(".")]
-    #print list
-    	preingredients = list.pop()
-	ingredients = []
-	for e in preingredients:
-        	if ":" in e:
-            		e.split(":")
-			ingredients.append(preingredients[1])
-	print ingredients
 
 read_file("fridge-fill-sample-input.txt")
-ingredients(read_file("fridge-fill-sample-input.txt"))

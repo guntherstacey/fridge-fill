80e0bb7ac422352b7fa623994963b184c144656e
Your Name <you@example.com>
2018-06-07 10:43:10 -0400

    Update name of file to run-fridge-fill

---
+++ run-fridge-fill.py
@@ -0,0 +1,8 @@
+def read_file(text_file):
+  if ".txt" in text_file:
+    recipe_list = open(text_file)
+    opened_recipe_list = (recipe_list.read())
+    print opened_recipe_list
+  else:
+    print ("Invalid Input: Not a .txt file")
+read_file("fridge-fill-sample-input.txt")

0c7fc705032acbf418d3eb251330031965936249
Your Name <you@example.com>
2018-06-08 18:56:47 -0400

    Start code to make ingredient list from scratch

--- run-fridge-fill.py
+++ run-fridge-fill.py
@@ -2,7 +2,19 @@ def read_file(text_file):
   if ".txt" in text_file:
     recipe_list = open(text_file)
     opened_recipe_list = (recipe_list.read())
-    print opened_recipe_list
+    #print opened_recipe_list
+    return opened_recipe_list
   else:
     print ("Invalid Input: Not a .txt file")
-read_file("fridge-fill-sample-input.txt")
+
+def ingredients(recipes):
+  ingredients_block = recipes.splitlines()
+  for item in ingredients_block:
+    start = item[item.index("$") + 1]
+    stop = item[item.index("$", start)]
+    new_ingredients_block += item[start:stop]
+    print new_ingredients
+
+
+recipe_block = read_file("fridge-fill-sample-input.txt")
+just_ingredients = ingredients(recipe_block)

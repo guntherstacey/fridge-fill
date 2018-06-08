702dbae9284c3d36e49fa7ebc8c5e7938d957b47
guntherstacey <39128432+guntherstacey@users.noreply.github.com>
2018-06-08 15:18:21 -0400

    Remove comment

--- run-fridge-fill.py
+++ run-fridge-fill.py
@@ -2,7 +2,6 @@ def read_file(text_file):
   if ".txt" in text_file:
     recipe_list = open(text_file)
     opened_recipe_list = (recipe_list.read())
-    #print opened_recipe_list
     return opened_recipe_list
   else:
     print ("Invalid Input: Not a .txt file")
